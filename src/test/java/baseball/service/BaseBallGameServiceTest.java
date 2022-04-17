e8393b4d1863169c21386938a476ca57b5ff98c2
oeunkyoung <ou1112@naver.com>
2022-04-17 17:54:06 +0900

    docs : README.md 구현된 기능 체크

---
+++ src/test/java/baseball/service/BaseBallGameServiceTest.java
@@ -0,0 +1,9 @@
+package baseball.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseBallGameServiceTest {
+
+
+
+}
---
+++ src/test/java/baseball/util/ValidationTest.java
@@ -0,0 +1,21 @@
+package baseball.util;
+
+import baseball.constant.Message;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ValidationTest {
+
+    @DisplayName("Continue Input Validation Test - 에러 인풋일 때 ")
+    @ParameterizedTest
+    @ValueSource(strings = {"10", "te"})
+    public void continueInputValidationTest2(String input) {
+        assertThatThrownBy(() -> Validation.chkIsOneOrTwo(input))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage(Message.EXP_ONE_OR_TWO);
+    }
+
+}
